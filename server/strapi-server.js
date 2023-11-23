c8c6b722ecde767d07034b09fde3e062bbbccd71
SebastianGode <sebastian@gode-familie.de>
2023-11-23 09:56:23 +0000

    Rename

--- server/strapi-server.js
+++ server/strapi-server.js
@@ -1,13 +1,4 @@
 'use strict';
 
-module.exports = async ({ strapi }) => {
-  const actions = [
-    {
-      section: 'plugins',
-      displayName: 'Read',
-      uid: 'read',
-      pluginName: 'strapi-plugin-sso',
-    },
-  ];
-  await strapi.admin.services.permission.actionProvider.registerMany(actions);
+module.exports = ({ strapi }) => {
 };
